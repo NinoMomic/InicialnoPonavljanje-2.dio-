c315b79273479352d74cf190d0c7657c0be42063
NinoMomic <58861649+NinoMomic@users.noreply.github.com>
2023-09-23 14:52:16 +0200

    Add files via upload

---
+++ InicijalnoPonavljanje(2.dio)2.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InicijalnoPonavljanje(2.dio)2
+{
+    class GeometrijskiLik
+    {
+        int brojStranica;
+        double[] stranice = new double[10];
+
+        public void UcitajStranice()
+        {
+            for(int i = 0; i < brojStranica; i++)
+            {
+                Console.WriteLine("Upisite duljinu {0} stranice: ", i+1);
+                string s = Console.ReadLine();
+                stranice[i] = Convert.ToDouble(s);
+            }
+        }
+        public double Opseg()
+        {
+            double opseg = 0;
+            
+            for(int i = 0; i < brojStranica; i++)
+            {
+                opseg += stranice[i];
+
+            }
+            return opseg;
+        }
+
+        public int BrojStranica { get => brojStranica; set => brojStranica = value; }
+        public double[] Stranice { get => stranice; set => stranice = value; }
+    }
+    class Trokut : GeometrijskiLik
+    {
+        public Trokut()
+        {
+            BrojStranica = 3;
+        }
+    }
+
+    class Cetverokut : GeometrijskiLik
+    {
+        public Cetverokut()
+        {
+            BrojStranica=4;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Trokut trokut = new Trokut();
+            Cetverokut cetverokut = new Cetverokut();
+            Console.WriteLine("Izračunavanje opsega trokuta" + "i četverokuta\r\n");
+            Console.WriteLine("Upiši duljine stranica trokuta\r\n");
+            trokut.UcitajStranice();
+            Console.WriteLine("Opseg trokuta je: {0}", trokut.Opseg());
+
+            Console.WriteLine("Upiši duljine stranica četverokuta\r\n");
+            cetverokut.UcitajStranice();
+            Console.WriteLine("Opseg četverokuta je: {0}", cetverokut.Opseg());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ InicijalnoPonavljanje(2.dio)3.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InicijalnoPonavljanje(2.dio)3
+{
+    class Vozilo
+    {
+        bool Leti, Plovi;
+    
+        public bool Leti { get => Leti; set => Leti = value; }
+        public bool Plovi { get => Plovi; set => Plovi = value; }
+    
+        public void KudaVozi()
+        {
+            Console.WriteLine("Da li leti: " + Leti + "\n" + "Da li plovi: " + Plovi);
+        }
+    }
+    class Brod : Vozilo
+    {
+        public Brod()
+        {
+            Plovi = true;
+        }
+    }
+    class Zrakoplov : Vozilo
+    {
+        public Zrakoplov()
+        {
+            Leti = true;
+        }
+    }
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Brod brod = new Brod();
+            Zrakoplov zrakoplov = new Zrakoplov();
+    
+            Console.WriteLine("Brod");
+            brod.KudaVozi();
+            Console.WriteLine("Zrakoplov");
+            zrakoplov.KudaVozi();
+    
+            Console.ReadKey();
+        }
+    }
+}
